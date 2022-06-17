d8e78e9bf02eec43e50f4e4b26f5bcd0359701d9
Tim Platz <platztim19@gmail.com>
2022-06-17 12:23:59 -0400

    frontend build moved to backend

---
+++ build/precache-manifest.ae6651f1d1d181066ad37ed57c774fc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4356978ab9a2b2814a58",
+    "url": "/static/css/main.d5968ada.chunk.css"
+  },
+  {
+    "revision": "4356978ab9a2b2814a58",
+    "url": "/static/js/main.8446de57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18071cfebd2a7827effe",
+    "url": "/static/js/2.0afb434d.chunk.js"
+  },
+  {
+    "revision": "cc17517bd5160439c7a5047a0ccea13c",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.fd89b456c5d80d104977b1e540363ffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab3e1c94b45c2240e657",
+    "url": "/static/css/main.d5968ada.chunk.css"
+  },
+  {
+    "revision": "ab3e1c94b45c2240e657",
+    "url": "/static/js/main.0fb9b761.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18071cfebd2a7827effe",
+    "url": "/static/js/2.0afb434d.chunk.js"
+  },
+  {
+    "revision": "b3b7cadc2c4eab42d41368a4a2309c3d",
+    "url": "/index.html"
+  }
+];
